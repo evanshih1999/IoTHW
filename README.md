d2e5de213aaeef41ec5a92594b65a48cc9da8c78
Evan Shih <b07901015@ntu.edu.tw>
2021-11-28 01:33:40 +0800

    gRPC

--- README.md
+++ README.md
@@ -3,8 +3,26 @@
 ## How to run
 - Install project dependencies
 ```bash
+# Install protobuf compiler
+$ sudo apt-get install protobuf-compiler
+
+# Install buildtools
+$ sudo apt-get install build-essential make
+
+# Install python packages
 $ pip3 install -r requirements.txt
 ```
+- Compile protobuf schema to python wrapper
+```bash
+$ make
+```
+
+- Start the gRPC services
+```bash
+$ python3 fibonacci.py --ip 0.0.0.0 --port 8080
+$ python3 loggingserver.py --ip 0.0.0.0 --port 8888
+```
+
 - Run the backend server
 ```bash
 $ python3 server.py runserver 0.0.0.0:8000
--- tutorial/views.py
+++ tutorial/views.py
@@ -4,6 +4,19 @@ from rest_framework.response import Response
 from rest_framework import permissions
 import json
 
+import os
+import os.path as osp
+import sys
+BUILD_DIR = osp.join(osp.dirname(osp.abspath(__file__)), "../build/service/")
+sys.path.insert(0, BUILD_DIR)
+import argparse
+
+import grpc
+import fib_pb2
+import fib_pb2_grpc
+import log_pb2
+import log_pb2_grpc
+
 # Create your views here.
 class FibonacciView(APIView):
     permission_classes = (permissions.AllowAny,)
@@ -12,10 +25,33 @@ class FibonacciView(APIView):
         decoded = request.body.decode('utf-8')
         body = json.loads(decoded)
         order = body['order']
-        return Response(data={ 'echo': 'Retrieving Fibonacci ' + str(order) }, status=200)
+
+        host = f"{'127.0.0.1'}:{'8080'}"
+        print(host)
+        with grpc.insecure_channel(host) as channel:
+            stub = fib_pb2_grpc.FibCalculatorStub(channel)
+
+            request = fib_pb2.FibRequest()
+            request.order = order
+
+            response = stub.Compute(request)
+            print(response.value)
+        
+        return Response(data={ response.value }, status=200)
 
 class LogsView(APIView):
     permission_classes = (permissions.AllowAny,)
 
     def get(self, request):
-        return Response(data={ 'echo': 'Retrieving Logs' }, status=200)
+
+        host = f"{'127.0.0.1'}:{'8888'}"
+        print(host)
+        with grpc.insecure_channel(host) as channel:
+            stub = log_pb2_grpc.LogCalculatorStub(channel)
+
+            request = log_pb2.LogRequest()
+
+            response = stub.Compute(request)
+            print(response.history)
+        
+        return Response(data={ 'history': response.history[:] }, status=200)
